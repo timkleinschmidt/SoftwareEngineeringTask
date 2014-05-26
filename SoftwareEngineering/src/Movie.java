4af557e67585e2115d03c813710229e5fff77e62
timkleinschmidt <tim-kleinschmidt@hotmail.de>
2014-05-26 02:43:25 +0200

    Refactoring #8
    Polymorphie

--- SoftwareEngineering/src/Movie.java
+++ SoftwareEngineering/src/Movie.java
@@ -2,44 +2,41 @@ public class Movie {
 	public static final int CHILDRENS = 2;
 	public static final int REGULAR = 0;
 	public static final int NEW_RELEASE = 1;
-	private String _title;
+	private String _name;
 	private int _priceCode;
-	public Movie(String newtitle, int newpriceCode) {
-		_title = newtitle;
-		_priceCode = newpriceCode;
-	}
-	public int getPriceCode() {
-		return _priceCode;
-	}
-	public void setPriceCode(int arg) {
-		_priceCode = arg;
-	}
-	public String getTitle (){
-		return _title;
-	};
-	double getCharge(int daysRented) { 
-		double result = 0; 
-		switch (getPriceCode()) { 
-		case Movie.REGULAR: 
-			result += 2; 
-			if (daysRented > 2) 
-				result += (daysRented - 2) * 1.5; 
-			break; 
-		case Movie.NEW_RELEASE: 
-			result += daysRented * 3; 
-			break; 
-		case Movie.CHILDRENS: 
-			result += 1.5; 
-			if (daysRented > 3) 
-				result += (daysRented - 3) * 1.5; 
-			break; 
-		} 
-		return result; 
+	public Movie(String name, int priceCode) { 
+		_name = name; 
+		setPriceCode(priceCode); 
+	} 
+
+	public int getPriceCode() { 
+		return _price.getPriceCode(); 
 	} 
 	int getFrequentRenterPoints(int daysRented) { 
-		if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) 
-			return 2; 
-		else 
-			return 1; 
-	}
+		return _price.getFrequentRenterPoints(daysRented); 
+	} 
+
+	public void setPriceCode(int arg) { 
+		switch (arg) { 
+		case REGULAR: 
+			_price = new RegularPrice(); 
+			break; 
+		case CHILDRENS: 
+			_price = new ChildrensPrice(); 
+			break; 
+		case NEW_RELEASE: 
+			_price = new NewReleasePrice(); 
+			break; 
+		default: 
+			throw new IllegalArgumentException("Incorrect Price Code"); 
+		} 
+	} 
+	private Price _price; 
+
+	public String getTitle (){
+		return _name;
+	};
+	double getCharge(int daysRented) { 
+		return _price.getCharge(daysRented); 
+	} 
 }
